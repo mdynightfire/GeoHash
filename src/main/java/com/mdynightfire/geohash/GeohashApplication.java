fe4eaa487baeabe38759d1762aed5f4a75d2f010
mdynightfire <644047640@qq.com>
2020-05-29 07:38:34 +0800

    first commit

---
+++ src/main/java/com/mdynightfire/geohash/GeohashApplication.java
@@ -0,0 +1,13 @@
+package com.mdynightfire.geohash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeohashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeohashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdynightfire/geohash/GeohashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdynightfire.geohash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeohashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
